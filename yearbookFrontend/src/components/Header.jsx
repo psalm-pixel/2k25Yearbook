2c71fbd1a9e4ff713f0b017289ef43f6930b52c6
coderX-tech <adebusuyisamuel3@gmail.com>
2025-05-17 23:27:50 +0100

    fixed the navbar issue

--- yearbookFrontend/src/components/Header.jsx
+++ yearbookFrontend/src/components/Header.jsx
@@ -2,7 +2,6 @@
 import { FaBars} from "react-icons/fa6";
 import { RxCross2 } from "react-icons/rx";
 import { useState } from 'react';
-import { Menu, X, ChevronDown } from 'lucide-react';
 
 
 
@@ -34,7 +33,9 @@ export default function Header() {
       };
 
   return (
-    <div className='flex justify-between items-center p-4 font-[Quicksand] bg-slate-950 backdrop-blur-lg shadow-lg sticky top-0 z-50 text-[15px] opacity-90'>
+    <div className='flex flex-col md:flex-row justify-between items-center p-4 font-[Quicksand] bg-slate-950 backdrop-blur-lg shadow-lg sticky top-0 z-50 text-[15px] opacity-90'>
+        <div className="flex flex-row items-center justify-between w-full">
+
         <h1 className='text-xl text-gray-300'><span className='text-[18px]'>2K</span><span className='text-[#50C878]'>25</span></h1>
       {
        isOpen ? 
@@ -42,12 +43,13 @@ export default function Header() {
        :   
           <FaBars className='md:hidden text-gray-300 text-2xl hover:text-[#50C878] cursor-pointer transition duration-100' onClick={handleToggle}/>
       } 
-        <nav className="hidden md:flex items-center space-x-4">
+        </div>
+        <nav className="flex justify-between items-center space-x-4 ">
             <ul className="hidden md:flex space-x-4">
               <li>
                 <a
                   href=""
-                  className="text-white hover:text-gray-300 no-underline relative group"
+                  className="text-white hover:text-gray-300 text-nowrap no-underline relative group"
                 >
                   The Legacy
                   <span className="absolute left-0 right-0  h-[0.129rem] rounded mt-[21px] bg-[#50C878]  transform scale-x-0 group-hover:scale-x-100 transition-transform duration-250 ease-in-out"></span>
@@ -88,9 +90,11 @@ export default function Header() {
             <div key={index}>
               <a
                 href={item.href}
-                className="block py-2 px-4 text-sm hover:bg-gray-700"
+                className="block py-2 px-4 text-sm hover:scale-105 text-gray-300  transition duration-300"
               >
-                {item.name}
+               <span className="text-gray-300 hover:text-[#50C878]">
+                 {item.name}
+                </span>
               </a>
             </div>
           ))}
